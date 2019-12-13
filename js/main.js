00e71d9438c8e59d319be508eea2d1fbef3a6790
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-12-13 20:59:33 +0300

    add different prices for different selected sizes of pizza

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ function Order(size, price, total, quantity){
   this.total = total;
   this.quantity = quantity
 }
+$(document).ready(function(){
+  $("button#hwncart").click(function(){
+    var hwnvalue = $(".hawaiianmenuorder option:selected");
+    var hwnsize = hwnvalue.val()
+    if(hwnsize === "large"){
+      var hwnprice = 1000
+    }else if (hwnsize === "medium") {
+      var hwnprice = 700
+    }else if (hwnsize === "small"){
+       var hwnprice = 500
+    }else{
+      var hwnprice = 0
+    }
