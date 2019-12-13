7158eaea9a82e608b6eeb3693fefa771cc09b658
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-12-13 21:05:50 +0300

    replicate the first function for hawaiian pizza with bbq, veggie & marga

--- js/main.js
+++ js/main.js
@@ -39,3 +39,35 @@ $(document).ready(function(){
     $(".price1").text( result )
 
 });
+$("button#bbqcart").click(function(){
+    var value = $(".bbqmenuorder option:selected");
+    var size = value.val()
+    if(size === "large"){
+      var price = 1000
+    }else if (size === "medium") {
+      var price = 700
+    }else if (size === "small"){
+       var price = 500
+    }else{
+       var price = 0
+    }
+     if(size === "large"){
+       $("#bbqprice").append("<li>" + price + "</li>" )
+     }else if (size === "medium") {
+       $("#bbqprice").append("<li>" + price + "</li>" )
+     }else{
+       $("#bbqprice").append("<li>" + price + "</li>" )
+     }
+     var bbqOrder = new Order(size, price)
+     bbqOrder.price.push(price)
+     var quantity = $(".bbqquantity option:selected")
+     var newValue = parseInt(quantity.val());
+     function quantiFy(){
+       return bbqOrder.price * newValue
+     }
+     var result = quantiFy();
+     $(".prod2").text(bbqOrder.size + " Meatzza pizza")
+     $(".price2").text(  result )
+
+
+});
