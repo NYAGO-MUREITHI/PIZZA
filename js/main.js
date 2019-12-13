5b85f99ae2fb2184c3f7ae5cba39e1219dc5bfa8
NYAGO-MUREITHI <murraygoogz@gmail.com>
2019-12-13 21:08:06 +0300

    function to produce result of the total of the cart by adding the prices

--- js/main.js
+++ js/main.js
@@ -139,3 +139,83 @@ $("button#margcart").click(function(){
 $("button#checkout").click(function(){
  $(".final-order").fadeIn();
 });
+$("button#finish").click(function(){
+  var hwnvalue = $(".hawaiianmenuorder option:selected");
+  var hwnsize = hwnvalue.val()
+  if(hwnsize === "large"){
+    var hwnprice = 1000
+  }else if (hwnsize === "medium") {
+    var hwnprice = 700
+  }else if (hwnsize === "small"){
+     var hwnprice = 500
+  }else{
+    var hwnprice = 0
+  }
+  var hawOrder = new Order(hwnsize)
+  hawOrder.price.push(hwnprice)
+  var quantity = $(".hawaiianquantity option:selected")
+  var newValue = parseInt(quantity.val());
+  function hawquantiFy(){
+    return hawOrder.price * newValue
+  }
+  var hawresult = hawquantiFy();
+  var value = $(".bbqmenuorder option:selected");
+  var size = value.val()
+  if(size === "large"){
+    var price = 1000
+  }else if (size === "medium") {
+    var price = 700
+  }else if (size === "small"){
+     var price = 500
+  }else{
+     var price = 0
+  }
+  var bbqOrder = new Order(size, price)
+  bbqOrder.price.push(price)
+  var quantity = $(".bbqquantity option:selected")
+  var newValue = parseInt(quantity.val());
+  function bbqquantiFy(){
+    return bbqOrder.price * newValue
+  }
+  var bbqresult = bbqquantiFy();
+  var vegvalue = $(".vegmenuorder option:selected");
+  var vegsize = vegvalue.val()
+  if(vegsize === "large"){
+    var vegprice = 1000
+  }else if (vegsize === "medium") {
+    var vegprice = 700
+  }else if (vegsize === "small"){
+     var vegprice = 500
+  }else{
+     var vegprice = 0
+  }
+  var vegOrder = new Order(vegsize, vegprice)
+  vegOrder.price.push(vegprice)
+  var quantity = $(".veggiequantity option:selected")
+  var newValue = parseInt(quantity.val());
+  function vegquantiFy(){
+    return vegOrder.price * newValue
+  }
+  var vegresult = vegquantiFy();
+  var margvalue = $(".margmenuorder option:selected");
+  var margsize = margvalue.val()
+  if(margsize === "large"){
+    var margprice = 1000
+  }else if (margsize === "medium") {
+    var margprice = 700
+  }else if (margsize === "small"){
+     var margprice = 500
+  }else{
+     var margprice = 0
+  }
+  var marOrder = new Order(margsize, margprice)
+  marOrder.price.push(margprice)
+  var quantity = $(".margaquantity option:selected")
+  var newValue = parseInt(quantity.val());
+  function margquantiFy(){
+    return marOrder.price * newValue
+  }
+  var margresult = margquantiFy()
+  function addTotal(hawresult,bbqresult,vegresult,margresult){
+    return parseInt(hawresult) + parseInt(bbqresult) + parseInt(vegresult) + parseInt(margresult)
+  }
